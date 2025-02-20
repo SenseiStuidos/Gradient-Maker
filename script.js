5bb4fab6c58d9f3d56e328ece6f737adfe8a789f
SenseiStuidos <mrlegendyt366@gmail.com>
2025-02-20 17:26:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById('create-gradient').addEventListener('click', function() 
     
     gradientDisplay.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
 });
+// Select all anchor tags
+const links = document.querySelectorAll('a');
+
+// Loop through each link and add target="_blank"
+links.forEach(link => {
+  link.setAttribute('target', '_blank');
+});
